9c81d69c8c978719f980bf0fbc2077da8b8169cf
Felipe <felipecoelhof.10@gmail.com>
2025-12-17 19:02:41 -0300

    H2 database, test profile, JPA

--- src/main/java/com/Projeto/crud/entities/User.java
+++ src/main/java/com/Projeto/crud/entities/User.java
@@ -1,10 +1,14 @@
 package com.Projeto.crud.entities;
 
-
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+
+
+@Entity
+@Table(name = "tb-user")
 
 @AllArgsConstructor
 @Getter
@@ -12,6 +16,8 @@ import lombok.Setter;
 @EqualsAndHashCode
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
